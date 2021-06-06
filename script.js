44f17613c79326959d8cc61e840ce15e789db148
Maria Macedo <mariamacedo2xx@gmail.com>
2021-06-06 12:11:59 -0300

    realizando a função de clicar

--- script.js
+++ script.js
@@ -5,8 +5,45 @@ let aviso = document.querySelector(".d-2");
 let lateral = document.querySelector(".d-1-right");
 let numeros = document.querySelector(".d-1-3");
 
+let etapaAtual = 0;
+let numero = "";
+
+function comecarEtapa() {
+  let etapa = etapas[etapaAtual];
+
+  let numerosHTML = "";
+
+  for (let i = 0; i < etapa.numeros; i++) {
+    if (i === 0) {
+      numerosHTML += '<div class="numero pisca"></div>';
+    } else {
+      numerosHTML += '<div class="numero "></div>';
+    }
+  }
+
+  seuVoto.style.display = "none";
+  cargo.innerHTML = etapa.titulo;
+  descriçao.innerHTML = "";
+  aviso.style.display = "none";
+  lateral.innerHTML = "";
+  numeros.innerHTML = numerosHTML;
+}
+
+function atualizaInterface() {}
+
 function clicou(n) {
-  alert("Clicou em " + n);
+  let elNumero = document.querySelector(".numero.pisca");
+  if (elNumero !== null) {
+    elNumero.innerHTML = n;
+    numero = `${numero}${n}`;
+
+    elNumero.classList.remove("pisca");
+    if (elNumero.nextElementSibling !== null) {
+      elNumero.nextElementSibling.classList.add("pisca");
+    } else {
+      atualizaInterface();
+    }
+  }
 }
 
 function branco() {
@@ -18,3 +55,5 @@ function corrige() {
 function confirma() {
   alert("Clicou em confirma");
 }
+
+comecarEtapa();
