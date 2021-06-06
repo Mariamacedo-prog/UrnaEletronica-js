eda47d4612bed6deed4a28ffe7e4aa8c40830522
Maria Macedo <mariamacedo2xx@gmail.com>
2021-06-05 23:54:02 -0300

    iniciando com a funcionalidade da urna

---
+++ script.js
@@ -0,0 +1,20 @@
+let seuVoto = document.querySelector(".d-1-1 span");
+let cargo = document.querySelector(".d-1-2 span");
+let descriçao = document.querySelector(".d-1-4");
+let aviso = document.querySelector(".d-2");
+let lateral = document.querySelector(".d-1-right");
+let numeros = document.querySelector(".d-1-3");
+
+function clicou(n) {
+  alert("Clicou em " + n);
+}
+
+function branco() {
+  alert("Clicou em branco");
+}
+function corrige() {
+  alert("Clicou em corrige");
+}
+function confirma() {
+  alert("Clicou em confirma");
+}
